992721c135e47c7d5d1d92c452934a146f9ce57b
ipe <izaki.yb1998@gmail.com>
2024-02-24 07:43:12 +0900

    goroutine loop good

--- main.go
+++ main.go
@@ -9,12 +9,13 @@ func main() {
 
 	var wg sync.WaitGroup
 	for i := 0; i < 10; i++ {
+		// iを別の変数に入れる
+		v := i
 		wg.Add(1)
 		go func () {
 			defer wg.Done()
-			// goroutineの中でiを出力するときにはすでにforループが終わってることがある
-			// そうすると10しか出力しない
-			fmt.Println(i)
+			// vはメモリ領域を別で確保されているから正しく出力できるってことかな？
+			fmt.Println(v)
 		}()
 	}
 	wg.Wait()
