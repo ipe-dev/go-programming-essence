de65881f79b9029d38a6f74cab673dda5f4892ad
ipe <izaki.yb1998@gmail.com>
2024-02-24 07:17:42 +0900

    goroutine for loop bad

--- main.go
+++ main.go
@@ -1,23 +1,21 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 
-	// main関数が終了するとgoroutineも強制終了してしまう
-	// それを防ぐためにsync.WaitGroupを使ってgoroutineの終了を待つ
 	var wg sync.WaitGroup
-	// リファレンスカウンタを+1
-	wg.Add(1)
-	go func() {
-		// リファレンスカウンタを-1
-		defer wg.Done()
-		// 重い処理
-		print("重い処理2")
-	}()
-
-	// 別の重い処理
-	print("重い処理1")
-	// リファレンスカウンタが0になるまで待つ
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func () {
+			defer wg.Done()
+			// goroutineの中でiを出力するときにはすでにforループが終わってることがある
+			// そうすると10しか出力しない
+			fmt.Println(i)
+		}()
+	}
 	wg.Wait()
 }
