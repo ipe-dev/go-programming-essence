c9f0dcfe92b20224ae05c276af45a7862a661722
ipe <izaki.yb1998@gmail.com>
2024-03-01 06:07:15 +0900

    リスト4.9 path/filepathパッケージの使用例

--- main.go
+++ main.go
@@ -2,10 +2,32 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
 )
 
 func main() {
-	now := time.Now()
-	fmt.Println(now.Format(time.DateTime))
+	files := []string{}
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = filepath.WalkDir(cwd, func(path string, info fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			// 隠しディレクトリをスキップする
+			if info.Name()[0] == '.' {
+				return fs.SkipDir
+			}
+			return nil
+		}
+		files = append(files, path)
+		return nil
+	})
+	fmt.Println(files)
 }
