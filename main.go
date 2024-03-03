1b6d50b6fa6dce804af6515336e97077f77c7a23
ipe <izaki.yb1998@gmail.com>
2024-03-03 16:49:31 +0900

    リスト4.17 Functional Option Pattern

--- main.go
+++ main.go
@@ -2,19 +2,13 @@ package main
 
 import (
 	_ "embed"
-	"net/http"
-
-	"github.com/labstack/echo/v4"
+	"go-programming-essence/server"
+	"log"
 )
 
-//go:embed static/logo.png
-var contents []byte
-
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.Blob(http.StatusOK, "image/png", contents)
-	})
-
-	e.Logger.Fatal(e.Start(":8989"))
+	svr := server.New("localhost", 8888)
+	if err := svr.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
