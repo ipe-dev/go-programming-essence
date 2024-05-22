ce56aba893e8dea0f015683e6cc1a76499f79222
ipe <izaki.yb1998@gmail.com>
2024-05-23 05:56:14 +0900

    goroutine

--- chapter6/goroutine/main.go
+++ chapter6/goroutine/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"sync"
 	"time"
 )
 func doSomething(wg *sync.WaitGroup) {
 	defer wg.Done()
 	time.Sleep(100 * time.Second)
+	for i := 0; i< 10000; i++ {
+		fmt.Printf("%d\n", i)
+	}
 }
 
 func main() {
